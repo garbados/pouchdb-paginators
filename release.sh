b840f4035e95c14b754df1735eb31013a542e18f
Diana Thayer <garbados@gmail.com>
2022-04-29 19:33:23 -0700

    it's my party and i can commit if i want to

--- release.sh
+++ release.sh
@@ -14,14 +14,21 @@ if [[ ! " ${OPTIONS[*]} " =~ " ${ACTUAL} " ]]; then
     exit 1
 fi
 
-echo Bumping version...
-npm version $ACTUAL -m 'version bump'
-echo Building artifacts...
-npm run build
-npm run build:min
-echo Pushing to origin...
-# git push
-# git push --tags
-echo Publishing to NPM
-npm publish --dry-run
+echo Cleaning directory... && \
+git stash && \
+git clean -dxf && \
+echo Installing dependencies... && \
+npm i && \
+echo Running tests... && \
+npm test && \
+echo Bumping version... && \
+npm version $ACTUAL -m 'version bump' && \
+echo Building artifacts... && \
+npm run build && \
+npm run build:min && \
+echo Pushing to origin... && \
+git push && \
+git push --tags && \
+echo Publishing to NPM && \
+npm publish --dry-run && \
 echo Done!
